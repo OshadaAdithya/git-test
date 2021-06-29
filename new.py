d2d2497c4e0968de0aab5c085dbbb22b2dbfeca9
Oshada <oshadaadithya@gmail.com>
2021-06-29 18:55:57 +0530

    first change

---
+++ new.py
@@ -0,0 +1,12 @@
+the_text=input("Enter some text.\n")
+
+#print-version1
+print('This is what you entered:')
+print(the_text)
+
+#print-version
+print('This is what you entered:',the_text)
+
+#print-version3- to supress printing of a new line,use end=''
+print('This is what you entered:',end='')
+print(the_text)
